4fb108a0de0e99ceec0d11369e7c0859b2934320
Viviana Cuartas Cardona <vivianacc9714@gmail.com>
2025-08-13 20:54:02 -0500

    Mejoras en inicio sesion

--- inicioSesion.js
+++ inicioSesion.js
@@ -1,32 +1,28 @@
 let intentoMax = 3;
 
 function inicioSesion() {
-    let user = prompt("Ingrese su usuario:");
-    let password = prompt("Ingrese su contraseña:");
+    
     
     for (let conIntentos = 1; conIntentos <= intentoMax; conIntentos++) {
+        let user = prompt("Ingrese su usuario:");
+        let password = prompt("Ingrese su contraseña:");
+
         if (user === "admin" && password === "1234") {
             console.log("Inicio de sesión exitoso.");
             return true; 
-        }
-      
+        } else{
         console.log(`Intento ${conIntentos} de ${intentoMax}.`);
         alert(`Usuario o contraseña incorrectos Intente nuevamente`)
-        console.log(`Error: Usuario o contraseña incorrectos. Quedan ${intentoMax - conIntentos} intentos.`);
-        
+        console.log(`Error: Usuario o contraseña incorrectos. Quedan ${intentoMax - conIntentos} intentos.`)};
+        git
         if (conIntentos === intentoMax) {
             alert("Cuenta bloqueada por 24 horas, comunícate con tu banco");
             console.log("Cuenta bloqueada por 24 horas, comunícate con tu banco");
             return false; 
         } 
-
-        
         user = prompt("Ingrese su usuario:");
         password = prompt("Ingrese su contraseña:");
     }
     return false;
 }
-
-if (inicioSesion()) {
-    ejecutarSistemaBancario();
-}
+inicioSesion()
